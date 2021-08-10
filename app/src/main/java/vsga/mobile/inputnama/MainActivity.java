4f75bce6f8f30527800e4ec6269e44d15c7046d8
Lenovo <ifaalifa24@gmail.com>
2021-08-10 20:04:27 +0700

    update fungsi input nama

--- app/src/main/java/vsga/mobile/inputnama/MainActivity.java
+++ app/src/main/java/vsga/mobile/inputnama/MainActivity.java
@@ -3,12 +3,37 @@ package vsga.mobile.inputnama;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText InputNama;
+    TextView hasilnya;
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        InputNama = (EditText)findViewById(R.id.InputNama);
+        hasilnya = (TextView)findViewById(R.id.tampil);
+        btn1 = (Button)findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                hasilnya.setText("Nama Anda: "+ InputNama.getText());
+            }
+        });
     }
+
+
+
+//    public void TampilNama(View v){
+//        label1.setText("Nama Anda: "+ TextNama.getText());
+//    }
 }
